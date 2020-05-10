4e3102a3ca40c4359941fdfe25c254b694df3e7f
Benjamin Crews <aceF22@gmail.com>
2020-05-10 14:33:36 -0500

    Update conf.py
    Versioned the docs to match the PyPI version.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ author = 'Benjamin Crews'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.6'
+release = '0.0.7'
 
 
 # -- General configuration ---------------------------------------------------
